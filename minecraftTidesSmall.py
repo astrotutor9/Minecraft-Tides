8e811734e828128e0143247a5349cf416f20f3d1
Robert Wiltshire <37550330+astrotutor9@users.noreply.github.com>
2019-08-27 13:15:18 +0100

    Add files via upload

--- minecraftTidesSmall.py
+++ minecraftTidesSmall.py
@@ -1,51 +1,56 @@
 from mcpi import minecraft
-from mcpi import block
+from time import time
 import requests
 
-# Get the unix epoch time from https://www.epochconverter.com/
-# Place it in the time = XXXX below.
+# Calculate the unix epoch time from Jan 1st 1070
 
-time = 1566572574
+time_now = str(time())
 
-# Find the latitude and longitude of a location.
-# Default values are for Newquay.
+# Find the latitude and longitude location from Google.
+# These values are for Newquay, Cornwall, UK.
 
-lat = 50.4
-lon = -5.0
+latitude = str(50.4155)
+longitude = str(-5.0737)
 
 # Get the latest tide height from closest location near yours
-# response = requests.get("https://www.worldtides.info/api?heights&lat&lon=&start=time&length=1&key=c51a887d-2c77-4437-8fcc-b189da284a7d")
-response = requests.get("https://www.worldtides.info/api?heights&lat&lon&start=1566579774&length=1&key=c51a887d-2c77-4437-8fcc-b189da284a7d")
+# Next two lines of code are one line.
+# Replace myKey with your long API key from worldtides.info
+
+address = "https://www.worldtides.info/api?heights&lat=" + latitude + "&lon=" + longitude + "&" + "start=" + time_now + "&length=1&key=" + "myKey"
+
+print(address)
+
+# Get the latest tide height from closest location near yours
+
+response = requests.get(address)
 data = response.json()
 
-print(response)
 print(data)
 
 tide_height = (data["heights"][0]['height'])
 
-# The +3 brings tide levels equal to minecraft level
-# Minecraft level is treated as Lowest Low Water so a High Tide
-# will be about 6 blocks deep of water.
+# The +4 brings sets medium tide levels to Minecraft level
+# Normal Minecraft level is treated as Lowest Low Water 
+# So a High Tide will be about 8 blocks deep of water.
 
-minecraftTide = (int(round((tide_height*10), 0))) + 3 
+minecraftTide = (int(round((tide_height+4), 0))) 
 
 print(minecraftTide)
-"""
+
 # Make link into Minecraft
+
 mc=minecraft.Minecraft.create()
 
-mc.postToChat("Tide was out.")
-mc.postToChat("Let's see how far in it is")
+mc.postToChat("Let's see how far in the tide is")
 
 # Find player position and alter water levels 60 blocks around them
 
 x,y,z = mc.player.getTilePos()
 
-for goingOut in range(1, minecraftTide+1, 1):
+for goingOut in range(1, minecraftTide):
     for xEdge in range(x-30, x+30):
         for zEdge in range(z-30, z+30):
             airOrNot = mc.getBlock(xEdge, goingOut, zEdge) # is there air? 
-            print(xEdge, goingOut, zEdge, airOrNot) # Just to show something happening
+            print(xEdge, goingOut, zEdge, airOrNot) # show check being made
             if airOrNot == 0:
                 mc.setBlock(xEdge, goingOut, zEdge, 8) # if air place water
-"""
